8b894054a9e7a5797f337010c913808b0ac5de69
zario <119659527+aixorne@users.noreply.github.com>
2026-08-14 09:32:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     content="width=device-width, initial-scale=1.0"
 >
 
-<title>Wage Calculator V6</title>
+<title>Wage Calculator V7</title>
 
 <link
     rel="stylesheet"
@@ -43,7 +43,7 @@
     </p>
 
     <span class="version">
-        WAGE CALCULATOR V6
+        WAGE CALCULATOR V7
     </span>
 
 </section>
